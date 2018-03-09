b3a7a6219982974a5a30acce95705f60394f458f
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-03-09 08:59:14 +0600

    Applet Codes

---
+++ Applet/Home/First.java
@@ -0,0 +1,12 @@
+import java.applet.*;
+import java.awt.*;
+
+public class First extends Applet{
+
+	public void paint(Graphics g)
+	{
+		g.setColor(Color.green);
+		g.setFont(new Font("Vrinda",Font.BOLD, 70));
+		g.drawString("RZS 12",250,250);
+	}
+}
