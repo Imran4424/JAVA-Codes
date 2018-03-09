d0b5e2547f92ddead09cc28aa7365520cf1d174e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-03-09 09:14:20 +0600

    Basic Codes

---
+++ Basic Codes/Box.java
@@ -0,0 +1,30 @@
+//package imu.java;
+
+import java.util.Scanner;
+
+class Box{
+	private double height,width,length;
+
+
+	public Box(){
+		height = width = length = 0.0;
+	}
+
+	public Box(double height,double width,double length){
+		this.height = height;
+		this.width = width;
+		this.length = length;
+	}
+	public void setValue(){
+		Scanner cin = new Scanner(System.in);
+		System.out.println("Input Box height,width and length seqencially");
+
+		height = cin.nextDouble();
+		width = cin.nextDouble();
+		length = cin.nextDouble();
+	}
+	public double display(){
+		return height*width*length;
+	}
+
+}
