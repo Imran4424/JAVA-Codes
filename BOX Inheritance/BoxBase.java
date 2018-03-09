0aa33c22ac039e309aab5a998673b0e142b27e9e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-03-09 09:16:43 +0600

    Inheritance Codes

---
+++ BOX Inheritance/BoxBase.java
@@ -0,0 +1,12 @@
+//package imu.java;
+
+class BoxBase{
+	
+	public static void main(String[] args) {
+		Box obj = new Box();
+		obj.setValue();
+
+		System.out.println("Volume is: " + obj.display());
+	}
+	
+}
